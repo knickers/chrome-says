515712dede0fbdf40af4710c0b16cbb28a2ca86a
Nick Cox <nickcox1008@gmail.com>
2017-04-01 16:20:01 -0600

    Use svg paths as sensors

--- index.php
+++ index.php
@@ -1,75 +1,38 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="sensors.css">
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="main.js"></script>
 		<title>Chrome Says</title>
 		<style>
-			.button {
-				width: 50px;
-				height: 30px;
-				cursor: pointer;
-				border: 1px solid black;
-				text-align: center;
-				padding-top: 10px;
-				border-radius: 10px;
-				margin-bottom: 8px;
-				background-color: #3cc;
-			}
-			.button:hover {
-				background-color: #5dd;
+			button {
+				vertical-align: top;
 			}
 			.active {
-				background-color: #ff6
+				opacity: 0.6;
 			}
-			.active:hover {
-				background-color: #ff0;
+			#svg_sensors {
+				width: 640px;
+				height: 610px;
+				background: url('images/chrome.png') 0 0;
 			}
-			#chrome {
-				position: relative;
+			#svg_sensors path,
+			#svg_sensors ellipse {
+				fill: transparent;
+				stroke: transparent;
+				stroke-width: 0.05px;
 			}
-			#chrome > svg {
-				position: absolute;
-				width: 100%;
-				height: 100%;
-			}
+			/*
+			#svg_sensors #path_green:hover { fill: green; }
+			#svg_sensors #path_red:hover { fill: red; }
+			#svg_sensors #path_yellow:hover { fill: yellow; }
+			#svg_sensors #path_blue:hover { fill: blue; }
+			*/
 		</style>
 	</head>
 	<body>
 		<div class="button" id="spin">Spin</div>
 		<div class="button" id="flash">Flash</div>
-		<div id="chrome">
-			<?php include 'images/drawing.svg" ?>
-			<!--
-			<img src="images/drawing.svg?d">
-			<ul id="sensors">
-				<li class="green n1"><a></a></li>
-				<li class="green n2"><a></a></li>
-				<li class="green n3"><a></a></li>
-				<li class="green n4"><a></a></li>
-				<li class="green n5"><a></a></li>
-				<li class="green n6"><a></a></li>
-				<li class="green n7"><a></a></li>
-				<li class="green n8"><a></a></li>
-				<li class="green n9"><a></a></li>
-				<li class="red n1"><a></a></li>
-				<li class="red n2"><a></a></li>
-				<li class="red n3"><a></a></li>
-				<li class="red n4"><a></a></li>
-				<li class="yellow n1"><a></a></li>
-				<li class="yellow n2"><a></a></li>
-				<li class="yellow n3"><a></a></li>
-				<li class="yellow n4"><a></a></li>
-				<li class="yellow n5"><a></a></li>
-				<li class="yellow n6"><a></a></li>
-				<li class="yellow n7"><a></a></li>
-				<li class="yellow n8"><a></a></li>
-				<li class="blue n1"><a></a></li>
-				<li class="blue n2"><a></a></li>
-				<li class="blue n3"><a></a></li>
-			</ul>
-			-->
-		</div>
+		<?= file_get_contents('images/sensors.svg') ?>
 	</body>
 </html>
