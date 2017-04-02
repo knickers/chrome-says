55f50ec190750b8a34f27b54051a8f2cce5201b5
Nick Cox <nickcox1008@gmail.com>
2017-04-02 17:12:00 -0600

    Buttons

--- index.php
+++ index.php
@@ -1,38 +1,28 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 		<script type="text/javascript" src="main.js"></script>
 		<title>Chrome Says</title>
 		<style>
-			button {
-				vertical-align: top;
+			#buttons {
+				position: absolute;
 			}
 			.active {
 				opacity: 0.6;
 			}
-			#svg_sensors {
-				width: 640px;
-				height: 610px;
-				background: url('images/chrome.png') 0 0;
+			#chrome_logo {
+				width: 100%;
+				max-width: 100%;
+				max-height: 100%;
 			}
-			#svg_sensors path,
-			#svg_sensors ellipse {
-				fill: transparent;
-				stroke: transparent;
-				stroke-width: 0.05px;
-			}
-			/*
-			#svg_sensors #path_green:hover { fill: green; }
-			#svg_sensors #path_red:hover { fill: red; }
-			#svg_sensors #path_yellow:hover { fill: yellow; }
-			#svg_sensors #path_blue:hover { fill: blue; }
-			*/
 		</style>
 	</head>
 	<body>
-		<div class="button" id="spin">Spin</div>
-		<div class="button" id="flash">Flash</div>
-		<?= file_get_contents('images/sensors.svg') ?>
+		<div id="buttons">
+			<button id="spin">Spin</button>
+			<button id="flash">Flash</button>
+		</div>
+		<?= file_get_contents('images/logo.svg') ?>
 	</body>
 </html>
