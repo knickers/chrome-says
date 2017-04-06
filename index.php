1b97cfc534339210c2767094eec13ceb9e039ff2
Nick Cox <nickcox1008@gmail.com>
2017-04-05 18:33:34 -0600

    Mobile viewports

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 		<script type="text/javascript" src="main.js"></script>
 		<title>Chrome Says</title>
